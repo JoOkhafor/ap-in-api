daddcc21b8dc179e604db1f13fe8dc7b2473aa0e
Apelete <adzohonouapelete@gmail.com>
2023-03-15 17:10:57 +0000

    user_auth_set

--- auth/auth.js
+++ auth/auth.js
@@ -1,42 +1,45 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 function Auth(req, res, next) {
     const authorization = req.headers.authorization;
     if (!authorization) {
         return res.status(401).json({
-            message: 'No Authorization Header'
-        })
+            message: "No Authorization",
+        });
     }
     try {
-        const token = authorization.split('Bearer ')[1];
+        const token = authorization.split("Bearer ")[1];
         if (!token) {
             return res.status(401).json({
-                message: 'Invalid Token Format'
-            })
+                message: "Invalid Token Format",
+            });
         }
         const decode = jwt.verify(token, SECRET_KEY);
-        req.user = decode
-        next()
+        req.user = decode;
+        next();
     } catch (error) {
         if (error instanceof jwt.TokenExpiredError) {
             return res.status(401).json({
-                message: 'Session Expired',
+                message: "Session Expired",
                 error: error.message,
-            })
+            });
         }
-        if (error instanceof jwt.JsonWebTokenError || error instanceof TokenError) {
+        if (
+            error instanceof jwt.JsonWebTokenError ||
+            error instanceof TokenError
+        ) {
             return res.status(401).json({
-                message: 'Invalid Token',
+                message: "Invalid Token",
                 error: error.message,
-            })
+            });
         }
         res.status(500).json({
-            message: 'Internal server Error',
+            message: "Internal server Error",
             error: error.message,
-            stack: error.stack
+            stack: error.stack,
         });
     }
 }
 
-module.exports = Auth
+module.exports = Auth;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
+const Auth = require("../auth/auth");
 const {
-  registerUser,
-  loginUser,
-  findUser,
-  getUsers,
+    registerUser,
+    loginUser,
+    findUser,
+    getUsers,
 } = require("../controllers/user.controller");
 
 const userRouter = Router();
@@ -12,5 +13,6 @@ userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/find/:userId", findUser);
 userRouter.get("/", getUsers);
+userRouter.get("/auth", Auth);
 
 module.exports = userRouter;
