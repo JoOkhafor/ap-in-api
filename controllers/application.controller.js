4ab9ef5c1260af5ca3dcf9b37c469c506d43e356
CoderZ <adzohonouapelete@gmail..com>
2023-04-03 16:51:41 +0000

    application route in index.js

--- controllers/application.controller.js
+++ controllers/application.controller.js
@@ -17,3 +17,45 @@ const getApplications = async (req, res) => {
 };
 
 module.exports = { Register, getApplications };
+
+
+const ApplicationRegister = async (req, res) => {
+  const { fullname, email, phone_number, location, profile, motivation } =
+    req.body;
+  if (
+    !fullname ||
+    !email ||
+    !phone_number ||
+    !location ||
+    !profile ||
+    !motivation
+  ) {
+    return res.status(405).send({ message: "Please fill in all the boxes!" });
+  }
+  if (!req.file)
+    return res.status(400).send({ message: "Please provide your CV file !" });
+  try {
+    const application = await ApplicationModel.findOne({ email });
+    if (application)
+      return res.status(402).send({
+        message: "Seems like someone with this email already applied !",
+      });
+    const newApplication = new ApplicationModel({
+      fullname,
+      email,
+      phone_number,
+      location,
+      cv: req.file.filename,
+      profile,
+      motivation,
+    });
+    const data = await newApplication.save();
+    // upload.single("file");
+    res.status(200).send({
+      ...data,
+      message: "Your appication is registered successfully !",
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
