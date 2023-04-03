ae6a95be4113fc689f04d32230cac577aeb4febc
CoderZ <adzohonouapelete@gmail..com>
2023-04-03 16:27:41 +0000

    Application model updated

--- models/application.model.js
+++ models/application.model.js
@@ -1,30 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const applicationSchema = new mongoose.Schema({
+const applicationSchema = new mongoose.Schema(
+  {
+    jobId: {
+      type: String,
+      required: true,
+    },
     fullname: {
-        type:String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type:String,
-        required: true
+      type: String,
+      required: true,
+    },
+    phone_number: {
+      type: String,
+      required: true,
     },
     location: {
-        type:String,
-        required: true
-    },
-    letter: {
-        type:String,
-        required: true
+      type: String,
+      required: true,
     },
     cv: {
-        type:String,
-        required: true
+      type: String,
+      required: true,
     },
-},{
+    profile: {
+      type: String,
+      required: true,
+    },
+    motivation: {
+      type: String,
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const ApplicationModel = mongoose.model('application.model', applicationSchema)
+const ApplicationModel = mongoose.model("application.model", applicationSchema);
 
-module.exports = ApplicationModel
+module.exports = ApplicationModel;
