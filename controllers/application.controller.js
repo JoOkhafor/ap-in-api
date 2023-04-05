7c94bdd1e917386cc5cfb61a218b3eb1dd07a04c
CoderZ <adzohonouapelete@gmail..com>
2023-04-05 17:16:54 +0000

    update

--- controllers/application.controller.js
+++ controllers/application.controller.js
@@ -1,45 +1,49 @@
 const ApplicationModel = require("../models/application.model");
+const { upload } = require("../uploads/multer");
 
 async function Register(req, res) {
-  console.log(req)
-  const { fullname, email, phone_number, location, profile, motivation } =
-    req.body;
-  if (
-    !fullname ||
-    !email ||
-    !phone_number ||
-    !location ||
-    !profile ||
-    !motivation
-  ) {
-    return res.status(405).send({ message: "Please fill in all the boxes!" });
-  }
-  if (!req.file)
-    return res.status(400).send({ message: "Please provide your CV file !" });
-  try {
-    const application = await ApplicationModel.findOne({ email });
-    if (application)
-      return res.status(402).send({
-        message: "Seems like someone with this email already applied !",
+  upload(req, res, async (req, res, err) => {
+    if(err) return res.status(500).send({message: err.message})
+    console.log(req);
+    const { fullname, email, phone_number, location, profile, motivation } =
+      req.body;
+    if (
+      !fullname ||
+      !email ||
+      !phone_number ||
+      !location ||
+      !profile ||
+      !motivation
+    ) {
+      return res.status(405).send({ message: "Please fill in all the boxes!" });
+    }
+    if (!req.file)
+      return res.status(400).send({ message: "Please provide your CV file !" });
+    try {
+      const application = await ApplicationModel.findOne({ email });
+      if (application)
+        return res.status(402).send({
+          message: "Seems like someone with this email already applied !",
+        });
+      const newApplication = new ApplicationModel({
+        fullname,
+        email,
+        phone_number,
+        location,
+        profile,
+        motivation,
+        cv: req.file.filename,
       });
-    const newApplication = new ApplicationModel({
-      fullname,
-      email,
-      phone_number,
-      location,
-      profile,
-      motivation,
-      cv: req.file.filename,
-    });
-    const data = await newApplication.save();
-    // upload.single("file");
-    res.status(200).send({
-      ...data,
-      message: "Your appication is registered successfully !",
-    });
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
+      const data = await newApplication.save();
+      // upload.single("file");
+      res.status(200).send({
+        ...data,
+        message: "Your appication is registered successfully !",
+      });
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  });
 }
 
 const getApplications = async (req, res) => {
