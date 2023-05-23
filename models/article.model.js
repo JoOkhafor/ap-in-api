809f90d3c9b06ba8d637e679902208b085fa7f16
CoderZ <adzohonouapelete@gmail.com>
2023-05-23 11:49:30 +0000

    article_model

---
+++ models/article.model.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require("mongoose");
+
+const articleSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    category: { type: String, required: true },
+    publish_date: { type: Date, required: true, default: new Date() },
+    author: { type: String, required: true },
+    details: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = model("articles", articleSchema)
