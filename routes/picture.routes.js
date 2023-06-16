2e24400e4f22473646e2470ece8cdb2a0ac2ac96
apelete21 <adzohonouapelete@gmail.com>
2023-06-16 07:05:47 +0000

    picture upload routes

---
+++ routes/picture.routes.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+const { pictureUpload } = require("../uploads/multer");
+const {
+  pictuteUploadMethod,
+  pictureDownloadMethod,
+  pictureDeleteMethod,
+} = require("../controllers/picture.controller");
+
+const router = Router();
+
+router.post("/upload", pictureUpload.single("file"), pictuteUploadMethod);
+router.get("/:filename", pictureDownloadMethod);
+router.get("delete", pictureDeleteMethod);
+
+module.exports = router;
