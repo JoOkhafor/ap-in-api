0dbb48b9db8e67b383ad6613f2a7548ae17ca131
apelete21 <adzohonouapelete@gmail.com>
2023-07-14 21:29:03 +0000

    users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
-const { registerUser, authUser } = require("../controllers/user.controller");
+const {
+  registerUser,
+  authUser,
+  updateUser,
+  removeUser,
+} = require("../controllers/user.controller");
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/auth", authUser);
+router.post("/update", updateUser);
+router.post("/delete", removeUser);
 
 module.exports = router;
