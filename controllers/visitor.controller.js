13b152cf7eba88b02501a58c687def7585754f7e
apelete21 <adzohonouapelete@gmail.com>
2023-08-28 21:28:48 +0000

    updates

--- controllers/visitor.controller.js
+++ controllers/visitor.controller.js
@@ -4,7 +4,7 @@ const { tokenCheck, createToken } = require("../utils/token");
 async function incrementVisitorCount(req, res) {
   const now = new Date().toISOString();
   const [year, month] = now.split("-");
-  const { page, token } = req.query;z
+  const { page, token } = req.query;
 
   if (!page) return res.status(200).end();
   const { ok } = tokenCheck(token);
